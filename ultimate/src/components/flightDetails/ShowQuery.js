c6e2aa221ccc135a95e5108879a4b6f43bb9686d
Chidinma Godwin <gchidex@yahoo.com>
2019-12-15 00:20:30 +0100

    Add a folder for the flight details components

---
+++ ultimate/src/components/flightDetails/ShowQuery.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ShowQuery extends React.Component {
+    constructor(){
+        super();
+        this.state= {
+
+        }
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <h1>This is the ShowQuery component</h1>
+            </React.Fragment>
+        )
+    }
+};
+
+export default ShowQuery;
