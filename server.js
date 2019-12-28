4218a469a49503a6f682ed5a8167b5ccea37fd74
Chidinma Godwin <gchidex@yahoo.com>
2019-12-28 05:09:04 +0100

    Use rest api instead of graphql to get the session key for the flight details from the response header

--- server.js
+++ server.js
@@ -2,41 +2,51 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
+//const bodyParser = require('body-parser');
 require("dotenv").config();
 
 // import required files
 const typeDefs = require("./typeDefs/typeDefs");
 const resolvers = require("./resolvers/resolvers");
+const sessionRoutes = require('./session');
 //const { resolvers } = require('./schema');
 
 const app = express();
 
 // middlewares
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.disable("x-powered-by");
+
+app.use('/skyscanner', sessionRoutes);
+
+app.get('/hello', (req,res)=> {
+  res.send('hello world')
+})
 
 // graphql server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req, res }) => {
-    res.set({ "Access-Control-Expose-Headers": "*" });
-  },
-  formatResponse: (response, requestContext) => {
-    const location = requestContext.context.res.location
-      ? requestContext.context.res.location
-      : "no location header";
-      response = Object.assign(response, {
-        extensions: {
-          headers: {
-            location: location
-          }
-        }
-      })
-      return response;
-  }
+  // context: ({ req, res }) => {
+  //   res.set({ "Access-Control-Expose-Headers": "*" });
+  // },
+  // formatResponse: (response, requestContext) => {
+  //   const location = requestContext.context.res.location
+  //     ? requestContext.context.res.location
+  //     : "no location header";
+  //   response = Object.assign(response, {
+  //     extensions: {
+  //       headers: {
+  //         location: location
+  //       }
+  //     }
+  //   });
+  //   return response;
+  // }
 });
 server.applyMiddleware({ app });
 
---
+++ session.js
@@ -0,0 +1,75 @@
+const router = require("express").Router();
+const axios = require("axios");
+const qs = require("qs");
+
+router.post("/", (req, res) => {
+  const {
+    inboundDate,
+    cabinClass,
+    children,
+    infants,
+    country,
+    currency,
+    locale,
+    originPlace,
+    destinationPlace,
+    outboundDate,
+    adults
+  } = req.body;
+  axios
+    .post(
+      "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
+      qs.stringify({
+        inboundDate,
+        cabinClass,
+        children,
+        infants,
+        country,
+        currency,
+        locale,
+        originPlace,
+        destinationPlace,
+        outboundDate,
+        adults
+      }),
+      {
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Origin": "*",
+          "x-rapidapi-host":
+            "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+          "x-rapidapi-key": "0c514d99b0mshbd049a32f91e13fp154323jsn1ac37a3785c9"
+        }
+      }
+    )
+    .then(response => {
+      console.log(response.headers.location);
+      res.send(response.headers.location);
+    })
+    .catch(error => {
+      if (error.response) {
+        /*
+         * The request was made and the server responded with a
+         * status code that falls out of the range of 2xx
+         */
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    } else if (error.request) {
+        /*
+         * The request was made but no response was received, `error.request`
+         * is an instance of XMLHttpRequest in the browser and an instance
+         * of http.ClientRequest in Node.js
+         */
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message);
+    }
+    console.log(error.config);
+      //res.send(error.request);
+    });
+});
+
+module.exports = router;
